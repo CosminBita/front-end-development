358ded1163d36176a0d46576e84bde234370bd99
Cosmin Bita <cosminbita79@gmail.com>
2022-02-06 15:49:49 -0500

    setTimeout function

--- JS-core/hello/hello.js
+++ JS-core/hello/hello.js
@@ -1,4 +1,13 @@
-{
+
+//ruleaza tellMe dupa 3000 ms//
+setTimeout(tellMe, 3000)
+
+//creates a new function named tellMe//
+function tellMe() {
     alert("hello, world!")
     alert("welcome to JavaScript!")
   }
+/*
+comments on multiple code lines
+*/
+//comments on a single code line//
