73eb634295354231dfbc4de90917cc7dd2c3cbb2
Cosmin Bita <cosminbita79@gmail.com>
2022-02-22 07:20:43 -0500

    commit all changes

--- JS-core/for/for.js
+++ JS-core/for/for.js
@@ -3,11 +3,24 @@ var i = 0;
 while (i < 5) {
   document.write(i);
   i++;
-} 
+}
 
-document.write('<br>')
+document.write("<br>");
 
 //FOR
-for (let i = 0; i < 5; i++) {
-  document.write(i);
+var i = 2;
+var j = 1;
+var max = 10;
+for (j = 1; j <= max; j++) {
+  message = `${j} x ${i} = ${j * i} <br>`;
+  document.write(message);
+}
+document.write('<br><br><br>')
+//IMBRICATED FOR LOOPS
+for (i = 1; i <= 10; i++) {
+  for (j = 1; j <= 10; j++) {
+    message = `${i} x ${j} = ${i * j} <br>`;
+    document.write(message)
+  }
+  document.write("<br>");
 }
